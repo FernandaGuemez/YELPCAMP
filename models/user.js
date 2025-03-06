b9e02e448d3ddfdfe36d6b61a93191e5e6a4d01f
FernandaG <fernanda.guemez@gmail.com>
2025-03-06 14:03:23 -0600

    Se suben archivos por primera vez a GitHub

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+// al usar este plugin, ya no necesitar definir en el esquema de UserSchema el password y el usuario, por lo que queda de esta manera:
+// y esto me sirve para la AUTENTIFICACION:
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("USer", UserSchema);
---
+++ seeds/index.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 30) + 10;
+    const camp = new Campground({
+      location: `${cities[random1000].city},${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, veniam tempora odit architecto enim aperiam. Quos hic, illum ut ratione inventore reiciendis a provident sint quod est possimus facere? Nisi.",
+      price,
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
