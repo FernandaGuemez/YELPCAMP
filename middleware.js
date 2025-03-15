5931a7b43d062459d97e3bc6acd7e29008eba774
FernandaG <fernanda.guemez@gmail.com>
2025-03-14 18:00:29 -0600

    Se agrega el comportamiento de ReturnTo, para acceder a la pagina que te estas loggeando y que no te envie de nuevo a campground por default

--- middleware.js
+++ middleware.js
@@ -1,8 +1,16 @@
 module.exports.isLoggedIn = (req, res, next) => {
+  //metodo que te permite autentificar al usuario y poder ingresar si está registrado o no para crear un nuevo campground*/
   if (!req.isAuthenticated()) {
-    //metodo que te permite autentificar al usuario y poder ingresar si está registrado o no para crear un nuevo campground*/
+    req.session.returnTo = req.originalUrl; // aqui se guarda el link original
     req.flash("error", "You must be signed in first!");
     return res.redirect("/login");
+  }
+  next();
+};
+
+module.exports.storeReturnTo = (req, res, next) => {
+  if (req.session.returnTo) {
+    res.locals.returnTo = req.session.returnTo;
   }
   next();
 };
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const { storeReturnTo } = require("../middleware");
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
@@ -37,19 +38,23 @@ router.get("/login", (req, res) => {
 
 router.post(
   "/login",
+  //use the storeReturnTo middleware to save the returnTo value from session to res.locals
+  storeReturnTo,
+  //passport.authenticate logs the user in and clears req.session
   passport.authenticate("local", {
     failureFlash: true /*va a flashear un mensaje para nosotros automaticamente*/,
     failureRedirect: "/login",
   }),
+  //now we can use res.locals.returnTo to redirect the user after login
   (req, res) => {
     req.flash("success", "Welcome back!");
-    res.redirect("/campgrounds");
+    const redirectUrl = res.locals.returnTo || "/campgrounds"; // update this line to use res.locals.returnTo
+    res.redirect(redirectUrl);
   }
 );
 
 //permitirá cerrar sesion
 //aqui se requeire pasar una funcion para que el método .logout funcione
-
 router.get("/logout", (req, res) => {
   req.logout(function (err) {
     if (err) {
