1d350910410758ae0f6d6314b7140363c7f7fd9b
FernandaG <fernanda.guemez@gmail.com>
2025-03-19 16:06:53 -0600

    Se agrega el middleware del autor y se integran todos los middleware de campground a un solo archivo middleware, se corrijen rutas

--- middleware.js
+++ middleware.js
@@ -1,3 +1,7 @@
+const { campgroundSchema, reviewSchema } = require("./schemas.js");
+const ExpressError = require("./utils/ExpressError.js");
+const Campground = require("./models/campground");
+
 module.exports.isLoggedIn = (req, res, next) => {
   //metodo que te permite autentificar al usuario y poder ingresar si está registrado o no para crear un nuevo campground*/
   if (!req.isAuthenticated()) {
@@ -13,4 +17,35 @@ module.exports.storeReturnTo = (req, res, next) => {
     res.locals.returnTo = req.session.returnTo;
   }
   next();
+};
+
+module.exports.validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+//middleeware de autorizacion de autor e id, para editar campgrounds, si no tu lo creaste, te arrojará you dont have permision
+module.exports.isAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground.author.equals(req.user._id)) {
+    req.flash("error", "You do not have permission to do that!");
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
+
+module.exports.validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
 };
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,21 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
-const { campgroundSchema } = require("../schemas.js");
-const { isLoggedIn } = require("../middleware.js");
+const {
+  isLoggedIn,
+  isAuthor,
+  validateCampground,
+} = require("../middleware.js");
 
-const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
-
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
 
 // campground index, es decir con toda la base de datos que tenemos de los campgrounds:
 
@@ -70,21 +62,28 @@ router.get(
 router.get(
   "/:id/edit",
   isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+
     if (!campground) {
       req.flash("error", "Cannot find that campground");
       return res.redirect("/campgrounds");
     }
+    //esta parte corresponde a permisos si el autor y el id es el mismo puedes editar
     res.render("campgrounds/edit", { campground });
   })
 );
 
 router.put(
   "/:id",
+  isLoggedIn,
+  isAuthor,
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
+
     const campground = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
     });
@@ -96,6 +95,7 @@ router.put(
 router.delete(
   "/:id",
   isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
