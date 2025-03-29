bb7c135e4796dbd646d1a9676400d29a9414e236
FernandaG <fernanda.guemez@gmail.com>
2025-03-29 17:49:30 -0600

    Agregamos la propiedad virtual Thumbnail para las imagenes cuando se edita el campground

--- models/campground.js
+++ models/campground.js
@@ -2,9 +2,16 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
+// esto sirve para que desde cloudinary podamos tener imagenes en tamaño miniatura y que desde la nube se haga el ajuste, es decir no está en nuestra base de datos la configuracion está virtual
+const ImageSchema = new Schema({ url: String, filename: String });
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
+
 const CampgroundSchema = new Schema({
   title: String,
-  images: [{ url: String, filename: String }],
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
